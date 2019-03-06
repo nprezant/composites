58ee5ebead0a9c71ee856d215f7670494d982ad0
nprezant <34352369+nprezant@users.noreply.github.com>
2019-03-05 23:01:07 -0500

    copy still doesn't work

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ class LaminateMaker(tk.Frame):
         headers = ['Layer', 'Orientation', 'Thickness', 'Material']
         self.table = EntryTable(self.master, headers=headers, rows=10, columns=4)
 
-        # bottom buttoms
+        # bottom frame
         self.bottom_frame = tk.Frame(self.master)
-        self.new_btn = tk.Button(self.bottom_frame, text='+New', command=self.table.add_row)
-        self.new_btn.grid(row=0, column=1, sticky='w')
+        self.status_bar = tk.Label(self.bottom_frame, text='OK')
+        self.status_bar.grid(row=0, column=1, sticky='w')
 
         # overall layout
         self.table.grid(row=0, column=0, sticky='nwe')
         self.bottom_frame.grid(row=2, column=0, sticky='we')
 
-        # toplevel menu
+        # top-level menu
         menubar = tk.Menu(self.master)
         menubar.add_command(label='New Layer', command=self.table.add_row)
         menubar.add_command(label='Quit!', command=self.master.quit)
@@ -32,8 +32,8 @@ class LaminateMaker(tk.Frame):
         # popup menu
         rclick = tk.Menu(self.master, tearoff=0)
         rclick.add_command(label='Add layer', command=self.table.add_row)
-        rclick.add_command(label='Delete layers', command=self.table.delete_rows)
-        rclick.add_command(label='Copy layer', command=lambda: print('copy'))
+        rclick.add_command(label='Delete layers', command=self.table.delete_selected_rows)
+        rclick.add_command(label='Copy layer', command=self.table.copy_selected_rows)
 
         def popup(event):
             rclick.post(event.x_root, event.y_root)
@@ -63,10 +63,14 @@ class EntryTable(tk.Frame):
 
     def add_row(self, count=1):
         '''Adds an empty row (or rows) to the bottom of the table'''
-        for _ in range(count):
-            new_row = self._make_row()
-            self._widgets.append(new_row)
-        self._re_order_widgets()
+        self.insert_row(len(self._widgets), count)
+
+
+    def copy(self, row):
+        '''Copies the row index provided and inserts it below'''
+        self.insert_row(row+1)
+        for j, cell in enumerate(self._widgets[row][1:]):
+            cell.text.set(self._widgets[row+1][j+1].text.get())
 
 
     def add_header_row(self, headers=None):
@@ -78,14 +82,28 @@ class EntryTable(tk.Frame):
         self._re_order_widgets()
 
 
-    def delete_rows(self):
-        '''Finds the selected rows and deletes them'''
-        for widget in self._widgets[1:]:
+    def selected_rows(self):
+        '''Generator for the selected rows'''
+        for row, widget in enumerate(self._widgets[1:]):
             if widget[0].var.get() == 1:
-                [cell.grid_forget() for cell in widget]
-                self._widgets.remove(widget)
+                yield row, widget
+
+
+    def delete_selected_rows(self):
+        '''Deletes the selected rows'''
+        for _, widget in self.selected_rows():
+            [cell.grid_forget() for cell in widget]
+            self._widgets.remove(widget)
         self._re_order_widgets()
 
+
+    def copy_selected_rows(self):
+        '''Copies the selected rows'''
+        for i, _ in self.selected_rows():
+            print(f'copy row {i}')
+            if not i == 0:
+                self.copy(i)
+        self._re_order_widgets()
 
 
     def _make_row(self):
@@ -100,7 +118,9 @@ class EntryTable(tk.Frame):
         current_row.append(c)
 
         for _ in range(self._columns):
-            entry = tk.Entry(self, textvariable=tk.StringVar(), borderwidth=0, width=10)
+            txt = tk.StringVar()
+            entry = tk.Entry(self, textvariable=txt, justify=tk.CENTER, borderwidth=0, width=10)
+            entry.text = txt
             current_row.append(entry)
         return current_row
 
@@ -123,6 +143,12 @@ class EntryTable(tk.Frame):
         '''Re numbers the widget grid locations based on the list order'''
         for row, widget in enumerate(self._widgets):
             for column, cell in enumerate(widget):
+
+                # update the layer count
+                if column == 1:
+                    if hasattr(cell, 'text'):
+                        cell.text.set(str(row))
+
                 cell.grid(row=row, column=column, sticky='nsew', padx=1, pady=1)
                 self.grid_columnconfigure(column, weight=1)        
 
