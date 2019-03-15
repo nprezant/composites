cf919d81784017b88bca1995a452e6d42c8e5a71
nprezant <34352369+nprezant@users.noreply.github.com>
2019-03-15 00:11:48 -0400

    make a lamina prop initial form

--- materialeditor.py
+++ materialeditor.py
@@ -20,14 +20,28 @@ class MaterialEditor(tk.Frame):
         self.title.grid(row=0, column=0, sticky='w')
         self.name_entry.grid(row=0, column=1, sticky='w')
 
+        # parameter entry
+
+        self.params = []
+        params = ['E1', 'E2', 'G12', 'v12']
+        operators = ['='] * 4
+        values = [''] * 4
+        self.lamina_props = BaseParametersFrame(
+            self.master, zip(params, operators, values))
+        # for varname in params:
+        #     self.params.append(InputParameterFrame(self.master, varname, '=', ''))
+
         # bottom frame
         self.bottom_frame = tk.Frame(self.master)
-        self.status_bar = tk.Label(self.bottom_frame, text='OK')
+        self.status_bar = tk.Label(self.bottom_frame, text='')
         self.status_bar.grid(row=0, column=1, sticky='w')
 
         # overall layout
         self.top_frame.grid(row=0, column=0, sticky='nwe')
-        self.bottom_frame.grid(row=1, column=0, sticky='we')
+        self.lamina_props.grid(row=1, column=0, sticky='we')
+        # for i, param in enumerate(self.params):
+        #     param.grid(row=i+1, column=0, sticky='nwe')
+        self.bottom_frame.grid(row=None, column=0, sticky='we')
 
         self.master.rowconfigure(0, weight=1)
         self.master.columnconfigure(0, weight=1)
@@ -61,6 +75,104 @@ class MaterialEditor(tk.Frame):
 
     def close_event(self, event=None):
         self.master.quit()
+
+
+class BaseParametersFrame(tk.Frame):
+    '''Base frame for the input parameters'''
+
+    def __init__(self, parent, params):
+        '''Initialize the parameters
+        params is a tuple: (varname, operator, value)
+        e.g. ('E1', '=', '100')'''
+        super().__init__(parent)
+
+        # make widgets
+        self._widgets: InputParameterFrame = []
+        for var, op, val in params:
+            self._widgets.append(
+                InputParameterFrame(self.master, var, op, val)
+                )
+
+        # grid widgets
+        for param in self._widgets:
+            param.grid(row=None, column=0, sticky='nwe')
+        
+
+
+class InputParameterFrame(tk.Frame):
+    '''Frame for an input parameter'''
+
+    def __init__(self, parent, varname, operator='=', value=''):
+        '''Initialize the table'''
+        super().__init__(parent)
+
+        # variable name
+        txt = tk.StringVar()
+        txt.set(varname)
+        self._varname = tk.Label(self, textvariable=txt, borderwidth=0, width=10)
+        self._varname.text = txt
+
+        # variable operator
+        txt = tk.StringVar()
+        txt.set(operator)
+        self._operator = tk.Label(self, textvariable=txt, borderwidth=0, width=10)
+        self._operator.text = txt
+
+        # variable value
+        txt = tk.StringVar()
+        txt.set(value)
+        self._value = tk.Entry(self, textvariable=txt, borderwidth=0, width=10)
+        self._value.text = txt
+
+        # grid positions
+        self._varname.grid(row=0, column=0, sticky='w')
+        self._operator.grid(row=0, column=1, sticky='w')
+        self._value.grid(row=0, column=2, sticky='we', pady=2)
+
+
+    @property
+    def varname(self):
+        '''Name of this variable'''
+        return self._varname.text.get()
+
+
+    @varname.setter
+    def varname(self, value):
+        '''Name of this variable'''
+        return self._varname.text.set(value)
+
+
+    @property
+    def operator(self):
+        '''Name of this variable's operator'''
+        return self._operator.text.get()
+
+
+    @operator.setter
+    def operator(self, value):
+        '''Name of this variable's operator'''
+        return self._operator.text.set(value)
+
+
+    @property
+    def value(self):
+        '''Name of this variable's value'''
+        return self._value.text.get()
+
+
+    @value.setter
+    def value(self, value):
+        '''Name of this variable's value'''
+        return self._value.text.set(value)
+
+
+    def widgets(self):
+        '''List of widgets in this frame'''
+        return [self._varname, self._operator, self._value]
+
+
+
+
 
 
 if __name__ == '__main__':
